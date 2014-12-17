59187af6c775afa34cc3c3236eece3be3ce141c2
Kannoken <imilya95@mail.ru>
2014-12-18 03:30:41 +0700

    mat.logic
    Порядок обработки деталей, подсчет трудоемкости

---
+++ mat.log/mlita.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+using namespace std;
+#include <iostream>
+int main()
+{
+	int n,k,i,j;
+	cout << "Enter count of machine" ; 
+	scanf("%d", &n); 
+	cout << "Enter count of detail";
+	scanf("%d", &k); 
+	int arr[n][k], ans[k]; 
+	cout << "Enter num of t"; 
+	for (i=0;i<k;i++)
+	{
+		for (j=0;j<2;j++)
+		{
+			scanf("%d", &arr[i][j]); 
+		}
+		cout << "\n";
+	}
+	for ()
+
+return 0;
+}
